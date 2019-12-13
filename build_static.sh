006bb950db4b8551ca95df0049061ea6a2bd0d60
Chris Dzombak <chris@dzombak.com>
2019-12-13 14:51:18 -0500

    fix build_static.sh

--- build_static.sh
+++ build_static.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ### Compile and link statically
-CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static" -w -s' mastopurge.go
+CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-extldflags "-static" -w -s' mastopurge.go api.go
